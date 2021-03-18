44fca685a707247111195f97e559c5ee51650f83
ananyacleetus <ananyacleetus@gmail.com>
2021-03-18 14:59:23 -0400

    fixed callback

--- public/lib/map.js
+++ public/lib/map.js
@@ -7,10 +7,23 @@ import "..//css/map.css";
 
 function Map(props) {
   var API_KEY = KEYS.GOOGLE_API_KEY;
-  var mapQuery = "UIUC";
+  var mapQuery = "UIUC"; //These props are sent from the callback functions from the sidebar to the main layout to the map
+  //They can be used to indicate which crime types should be displayed
+
+  var burglary = props.burglary;
+  var idUse = props.idUse;
+  var theftUnder = props.theftUnder;
+  var theftOver = props.theftOver;
+  var harrassment = props.harrassment;
+  var propertyDamage = props.propertyDamage;
+  var tresspassing = props.tresspassing;
+  var sexualAssault = props.sexualAssault;
+  var prostitution = props.prostitution;
+  var underageLiquor = props.underageLiquor; // console.log(burglary);
+
   return /*#__PURE__*/React.createElement("div", {
     id: "fullChart"
-  }, /*#__PURE__*/React.createElement("p", null, "Testing Callback:"), /*#__PURE__*/React.createElement("iframe", {
+  }, /*#__PURE__*/React.createElement("iframe", {
     id: "map",
     loading: "lazy",
     allowFullScreen: true,
--- src/map.js
+++ src/map.js
@@ -11,12 +11,26 @@ function Map(props) {
   const API_KEY = KEYS.GOOGLE_API_KEY;
   const mapQuery = "UIUC";
 
+
+  //These props are sent from the callback functions from the sidebar to the main layout to the map
+  //They can be used to indicate which crime types should be displayed
+  const burglary = props.burglary;
+  const idUse = props.idUse;
+  const theftUnder = props.theftUnder;
+  const theftOver = props.theftOver;
+  const harrassment = props.harrassment;
+  const propertyDamage = props.propertyDamage;
+  const tresspassing = props.tresspassing;
+  const sexualAssault = props.sexualAssault;
+  const prostitution = props.prostitution;
+  const underageLiquor = props.underageLiquor;
+
+  // console.log(burglary);
+
+
     return (
 
         <div id="fullChart">
-
-          <p>Testing Callback:</p>
-
         <iframe
         id="map"
         loading="lazy"
