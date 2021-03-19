fdc71637563f359744072dee8a2d295128012268
ananyacleetus <ananyacleetus@gmail.com>
2021-03-19 02:11:26 -0400

    added list of TODOs

--- public/lib/map.js
+++ public/lib/map.js
@@ -10,8 +10,9 @@ var map;
 var overlayView;
 
 function MapContainer(props) {
-  // TODO: 
+  // TODO:
   // - Investigate Google Maps event listeners for mouseover/mouseout events and how to get data from them for the tooltip (regular tooltip won't work with gmaps)
+  //      - Example of Google Maps event listener: https://jsfiddle.net/svigna/VzYF6/
   // - Import police crime data and create a hardcoded array of each possible location's coordinates (we can then filter the data to show all crime at that location displayed at that coordinate)
   // - Create intro/title page explaining motivation and ethical issues and tools we used
   // - Fix projection (scaling from coordinates to pixels) so the points occur closer to the right spots on the map (can be done after police data is imported)
